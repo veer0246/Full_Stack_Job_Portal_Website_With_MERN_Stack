56ab267837e7c406ac5c77c1f4c2df878813a63d
veer0246 <veer009292@gmail.com>
2024-09-07 16:14:36 +0530

    create user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,55 @@ export const register = async (req, res) => {
     }
 }
 
+//User login---------------------------------------------------
+export const login = async (req, res) => {
+    try {
+        // access email, password and role from the body---
+        const { email, password, role } = req.body
+        if (!email || !password || !role) {
+            return res.status(400).json({ message: "Something is missing", success: false });
+        };
 
+        //check user email--------
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: 'Incorrect email or password', success: false })
+        };
+
+        //check user password------
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "Incorrect email or password", success: false })
+        };
+
+        // check user role --------
+        if (role != user.role) {
+            return res.status(400).json({ message: "Account doesn't exist with current role.", success: false })
+        }
+
+        // create token for expire your login in given time--------- 
+        const tokenData = {
+            userId: user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRATE_KEY, { expiresIn: '1d' });
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+
+        //create cookie to store token for security purpose--
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.name}`, user, success: true
+        })
+
+    } catch (error) {
+        return res.status(201).json({ message: "Error in login", success: false, error: message })
+    }
+}
 
 
